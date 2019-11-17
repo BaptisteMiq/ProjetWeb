e5fca011b0ba9cc46bccbad2b3760ae1c0ec501d
Baptiste <baptiste.miquel@viacesi.fr>
2019-11-17 15:08:52 +0100

    mostpopindex

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\Session;
 
 use App\Controller\SiteController;
+use App\Controller\ShopController;
 use App\Controller\EventController;
 
 use App\Acme\CustomBundle\API;
@@ -14,6 +15,12 @@ use App\Acme\CustomBundle\API;
 class MainController extends SiteController
 {
     public function index(Request $request) {
-        return $this->rendering('index.html.twig');
+
+        $categories = API::call('GET', '/shop/getCategoriesAndProducts');
+        $mostPop = ShopController::getMostPopularProducts();
+
+        return $this->rendering('index.html.twig', [
+            'mostPop' => $mostPop
+        ]);
     }
 }
